281a75b85c2042ea16ce74a54a8bfe69be920e3d
fadspadilla <fadspadilla@gmail.com>
2024-09-22 15:11:17 +0800

    updated the scripts for order and app js

--- app.js
+++ app.js
@@ -2,4 +2,17 @@ import { Order } from "./modules/order.js";
 
 const order = new Order();
 
-console.log(order.add())
+order.add("Choco sundae - 2, burger - 3")
+order.add("chicken - 4")
+order.add("spaghetti - 2")
+order.list();
+order.remove("burger - 1")
+order.list();
+order.remove("french fries - 1")
+order.list();
+order.remove("spaghetti - 2")
+order.list();
+order.remove("spaghetti - 2")
+order.list();
+order.modify("burger, burger steak")
+order.list();
--- modules/order.js
+++ modules/order.js
@@ -1,10 +1,63 @@
 export class Order {
     constructor() {
-        // 
+        this.items = {}
     }
 
-    add() {
-        console.log("Hello from order js")
+    add(addItem) {
+        const itemsArray = addItem.split(', '); //creates 2D array
+
+        itemsArray.forEach(item => {
+            const [name, quantity] = item.trim().split('-');
+            const qty = parseInt(quantity);
+            const nameCaps = name.trim().toUpperCase();
+            
+            if (this.search(nameCaps)) { //if it already exists just add the qty
+                this.items[nameCaps] += qty;
+            } else { //add unique key:value pair
+                this.items[nameCaps] = qty;
+            }
+        });
+    }
+
+    search(item) {
+        return this.items.hasOwnProperty(item);
+    }
+
+    remove(removeItem){
+        const itemsArray = removeItem.split(', '); //creates 2D array
+
+        itemsArray.forEach(item => {
+            const [name, quantity] = item.trim().split('-');
+            const qty = parseInt(quantity);
+            const nameCaps = name.trim().toUpperCase();
+
+            if (this.search(nameCaps)) {
+                this.items[nameCaps] -= qty;
+                if (this.items[nameCaps] <= 0) {
+                    delete this.items[nameCaps];
+                }
+            } else { //item doesn't exist
+                console.log(`Not Found: ${nameCaps}`);
+            }
+        })
+    }
+
+    modify(modifyItem){
+        const [oldName, newName] = modifyItem.split(', '); //creates 2D array
+        const oldNameCaps = oldName.trim().toUpperCase();
+        const newNameCaps = newName.trim().toUpperCase();
+
+        if (this.search(oldNameCaps)) {
+            this.items[newNameCaps] = this.items[oldNameCaps];
+            delete this.items[oldNameCaps];
+        } else { //item doesn't exist
+            console.log(`Not Found: ${oldNameCaps}`);
+        }
+        
+    }
+
+    list(){
+        console.log(this.items)
     }
 
 }
