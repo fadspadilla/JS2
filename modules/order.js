b33fa7728da25854aa875b6484bb0e05090b6e87
fadspadilla <98188121+fadspadilla@users.noreply.github.com>
2024-11-07 10:19:15 +0800

    Update order.js

--- modules/order.js
+++ modules/order.js
@@ -1,78 +1,26 @@
 export class Order {
-    constructor() {
-        this.items = {}
-    }
+  constructor() {
+    this.items = {};
+    this.tableDisplay = document.getElementById("item-lists");
+  }
 
-    add(addItem) {
-        const itemsArray = addItem.split(', '); //creates 2D array
+  add(itemstr) {
+    const itemsArray = itemstr.split(", "); //[Choco sundae - 2, burger - 3]
 
-        itemsArray.forEach(item => {
-            const [name, quantity] = item.trim().split('-');
-            const qty = parseInt(quantity);
-            const nameCaps = name.trim().toUpperCase();
-            
-            if (this.search(nameCaps)) { //if it already exists just add the qty
-                this.items[nameCaps] += qty;
-            } else { //add unique key:value pair
-                this.items[nameCaps] = qty;
-            }
-        });
-    }
+    itemsArray.forEach((item) => {
+      const [name, quantity] = item.trim().split("-");
+      const qty = parseInt(quantity);
+      const nameCaps = name.trim().toUpperCase();
 
-    search(item) {
-        // return this.items.hasOwnProperty(item);
-        return item in this.items;
-        // for(let key in this.items){
-        //     if(key == item){
-        //         return true;
-        //     }
-        // }
-        // return false;
-    }
+      if (this.search(nameCaps)) {
+        //if it already exists just add the qty
+        this.items[nameCaps] += qty;
+      } else {
+        //add unique key:value pair
+        this.items[nameCaps] = qty;
+      }
+    });
 
-    remove(removeItem){
-        const itemsArray = removeItem.split(', '); //creates 2D array
-
-        itemsArray.forEach(item => {
-            const [name, quantity] = item.trim().split('-');
-            const qty = parseInt(quantity);
-            const nameCaps = name.trim().toUpperCase();
-
-            if (this.search(nameCaps)) {
-                this.items[nameCaps] -= qty;
-                if (this.items[nameCaps] <= 0) {
-                    delete this.items[nameCaps];
-                }
-            } else { //item doesn't exist
-                console.log(`Not Found: ${nameCaps}`);
-            }
-        })
-    }
-
-    modify(modifyItem){
-        const [oldName, newName] = modifyItem.split(', '); //creates 2D array
-        const oldNameCaps = oldName.trim().toUpperCase();
-        const newNameCaps = newName.trim().toUpperCase();
-
-        if (this.search(oldNameCaps)) {
-            this.items[newNameCaps] = this.items[oldNameCaps];
-            delete this.items[oldNameCaps];
-        } else { //item doesn't exist
-            console.log(`Not Found: ${oldNameCaps}`);
-        }
-        
-    }
-
-    list(){
-        console.log(this.items)
-        // if (Object.keys(this.items).length === 0) {
-        //     console.log("Order is empty");
-        // } else {
-        //     console.log("Current Order:");
-        //     for (const [name, quantity] of Object.entries(this.items)) {
-        //         console.log(`${name} - ${quantity}`);
-        //     }
-        // }
-    }
-
+    this.list();
+  }
 }
